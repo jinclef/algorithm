17d42ea13a789fb0cff9dcb669f36ea2f67502a1
2021006317 <eve7777@hanyang.ac.kr>
2025-02-06 20:06:45 +0900

    6588: timeout

--- sds/20250206/6588.cpp
+++ sds/20250206/6588.cpp
@@ -6,8 +6,8 @@ typedef long long ll;
 
 ll t; // n
 ll a, b; // n = a+b, 홀수 소수
-bool visited[1000001];
-ll primes[1000001];
+bool visited[1000001]; // visited[i]=true면 i는 합성수.
+ll primes[1000001]; //소수 집합
 //vector<ll> primes;
 ll primecnt;
 
@@ -32,35 +32,38 @@ void setPrimes(ll n) {
 		}
 	}
 }
-ll mymax;
+ll mymax=-1;
 bool trygold(ll idxA, ll idxB, ll NminusA, ll n) { // back tracking 해서 당첨되면 바로 out
-	if (primes[idxB] > NminusA) return false;
-
 	bool res = false;
-	while (idxA < idxB && idxB >=0) {
+	while (idxA <= idxB && idxB >=0) { //idxA와 이룰 수 있는 쌍 찾기
 		ll nminusa = n - primes[idxA];
-		if (primes[idxA] <= 2 || nminusa <= 2) {
-			idxA++; continue;
+
+        if (primes[idxB] > nminusa) {
+            idxB--;
+            continue;
+        }
+
+		if (primes[idxA] <= 2 || nminusa <= 2) { // 홀수 소수의 합이어야 함. 즉 2는 안됨.
+			idxA++;
+            continue;
 		}
+
 		//cout << "while: " << primes[idxA] << ' ' << primes[idxB]  << ' ' << nminusa << '\n';
-		if (visited[nminusa]) { // 만들어야 되는 수가 짝수인 경우
+		if (visited[nminusa]) { // 만들어야 되는 수(b)가 합성수인 경우
 			idxA++; continue;
 		}
+
 		if (primes[idxB] == nminusa) {
 			a = primes[idxA];
 			b = primes[idxB];
 			res = true;
 			//cout << "hi? " << a << " + " << b << '\n';
-			mymax = max(mymax, b - a);
+			mymax = max(mymax, b - a); // b-a가 0인데?
 			break;
 		}
 		else {
 			idxB--;
 		}
-	}
-	if (idxA == idxB) return false;
-	else {
-		return true;
 	}
 	return res;
 }
@@ -70,15 +73,15 @@ int main() {
 	// 4보다 큰 모든 짝수는 두 홀수 소수의 합으로 나타낼 수 있다.
 	while (true) {
 		clearPrimeset();
-		cin >> t; // test cases
+		cin >> t; // t is even.
 		setPrimes(t);
 		if (t == 0) break;
 		/*for (ll i = 0; i < primecnt; i++) {
 			cout << primes[i] << ' ';
 		}
 		cout << '\n';*/
-		trygold(0, primecnt - 1, t, t);
-		if (mymax !=0) cout << a << " + " << b << '\n';
+		trygold(0, primecnt - 1, t-primes[0], t);
+		if (mymax !=-1) cout << t << " = " << a << " + " << b << '\n';
 		else cout << "Goldbach's conjecture is wrong." << '\n';
 	}
 	return 0;
