921499593af81a540bcbaa8f9e7ad9ab026c15d9
2021006317 <eve7777@hanyang.ac.kr>
2025-02-26 15:11:38 +0900

    2824: mat wae tle

--- sds/practices/2824.cpp
+++ sds/practices/2824.cpp
@@ -18,12 +18,14 @@ priority_queue<ll, vector<ll>, cmp> arr_m;
 
 ll n, m;
 vector<ll> factor;
-bool visited[1001];
+const ll MAX_VALUE = 1000000001; // 10^9 + 1
+const ll LIMIT_VALUE = 31625; // > sqrt(10^9)
+bool visited[LIMIT_VALUE];
 
 void eratos(){
-    for(int i=2;i<=1000;i++){
+    for(ll i=2;i <= LIMIT_VALUE;i++){
         if(visited[i]) continue;
-        for(int j=i*i;j<=1000;j+=i){
+        for(ll j=i*i;j<=LIMIT_VALUE;j+=i){
             visited[j]=true;
         }
         factor.push_back(i);
@@ -34,11 +36,11 @@ int main(){
     eratos();
 
     cin >> n;
-    for (int i=0;i<n;i++){
+    for (ll i=0;i<n;i++){
         ll na;
         cin >> na; // 다 곱하면 A. 즉 A의 인수..?
-        int j=0;
-        while(na>1){
+        ll j=0;
+        while(na>1 && j<factor.size()){
             if(na%factor[j]==0){ // 나눠떨어지면 그거 소인수임.
                 na/=factor[j];
                 arr.push(factor[j]);
@@ -49,11 +51,11 @@ int main(){
     }
 
     cin >> m;
-    for (int i=0;i<m;i++){
+    for (ll i=0;i<m;i++){
         ll ma;
         cin >> ma;
-        int j=0;
-        while(ma>1){
+        ll j=0;
+        while(ma>1 && j<factor.size()){
             if(ma%factor[j]==0){
                 ma/=factor[j];
                 arr_m.push(factor[j]);
@@ -63,31 +65,53 @@ int main(){
         }
     }
 
+    // while(arr.size()>0){
+    //     cout << arr.top() << ' ';
+    //     arr.pop();
+    // }
+
+    // cout << '\n';
+
+    // while(arr_m.size()>0){
+    //     cout << arr_m.top() << ' ';
+    //     arr_m.pop();
+    // }
+
     ll gcd = 1;
-    int i=0;
-    while(arr_m.size()>0 && arr.size()>0){
+    ll i=0;
+    while(arr_m.size()>0 && arr.size()>0 && i<factor.size()){
+        // cout << factor[i] << '\n';
         if(arr.top()==factor[i] && arr_m.top()==factor[i]){
             gcd *= factor[i];
             arr.pop();
             arr_m.pop();
         } else{
-            while(arr.top()<=factor[i]) arr.pop();
-            while(arr_m.top()<=factor[i]) arr_m.pop();
+            while(arr.size() > 0 && arr.top()<=factor[i]) {
+                // cout << "arr pop: " << arr.top();
+                arr.pop();
+            }
+            // cout << '\n';
+            while(arr_m.size() > 0 && arr_m.top()<=factor[i]) {
+                // cout << "arr_m pop: " << arr_m.top();
+                arr_m.pop();
+            }
+            // cout << '\n';
             i++;
         }
     }
-    // const int NINE = 1000000000;
-    // if(gcd >= NINE){
-    //     ll final_gcd = gcd % 1000000000;
-    //     string final;
-    //     int cnt = 9 - log10(final_gcd)/10;
-    //     for(int i=0; i<cnt; i++){
-    //         final += '0';
-    //     }
-    //     final += to_string(final_gcd);
-    //     cout << final << '\n';
-    // }
-
-    cout << gcd << '\n';
+    const int NINE = 1000000000;
+    if(gcd >= NINE){
+        ll final_gcd = gcd % 1000000000;
+        // cout << "final_gcd: " << final_gcd << '\n';
+        string final;
+        int cnt = 9 - to_string(final_gcd).length();
+        for(int i=0; i<cnt; i++){
+            final += '0';
+        }
+        final += to_string(final_gcd);
+        cout << final << '\n';
+    } else {
+        cout << gcd << '\n';
+    }
     return 0;
 }
