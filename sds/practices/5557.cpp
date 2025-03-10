e0e289fff34ca1ed627aff5f26a16a0f35df3a45
2021006317 <eve7777@hanyang.ac.kr>
2025-03-10 22:13:21 +0900

    5557: 맞는거 같은데 왜 안되지?

---
+++ sds/practices/5557.cpp
@@ -0,0 +1,78 @@
+// 1학년
+
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int subsum[101][2]; // subsum[i][0]: i번째 수까지의 (가능한) 누적합에 +[i+1] 했을 때 값, subsum[i][1]: i번째 수까지의 (가능한) 누적합에 -[i+1] 했을 때 값.
+ll subcnt[101][2]; // " 했을 때 개수.
+
+int N;
+ll sum;
+ll cnt;
+int nums[101];
+
+// ll makeSubsum(int newIdx, bool doPlus){
+//     if(newIdx >=N) return 0;
+
+//     if (doPlus) subcnt[newIdx][0] +=1; else subcnt[newIdx][1] += 1;
+
+//     ll plusX = sum + nums[newIdx];
+//     ll minusX = sum - nums[newIdx];
+//     bool canPlus = (plusX >= 0 && plusX <=20);
+//     bool canMinus = (minusX >= 0 && minusX <=20);
+
+
+//     if(canPlus) {
+//         if(subsum[newIdx][0] != -1) return subcnt[newIdx][0]; // 이미 계산된 경우
+//         if(subsum[newIdx][0] == subsum[newIdx][1]) return subcnt[newIdx][1]; // 이미 이전 재귀에서 계산된 경우
+//         subsum[newIdx][0] = plusX;
+//         return 1 + makeSubsum(newIdx + 1, true);
+//     }
+
+//     if(canMinus) {
+//         if(subsum[newIdx][1] != -1) return subcnt[newIdx][1]; // 이미 계산된 경우
+//         if(subsum[newIdx][1] == subsum[newIdx][0]) return subcnt[newIdx][0]; // 이미 이전 재귀에서 계산된 경우
+//         subsum[newIdx][1] = minusX;
+//         return 1 + makeSubsum(newIdx + 1, false);
+//     }
+
+//     if(!canPlus && !canMinus) return 0;
+// }
+
+int savedStepSum[101];
+
+ll totalCase(int curIdx, int curSum){
+    if(curIdx+1 >= N) return 1; // 기본 종료조건
+
+    ll partSum = curSum + nums[curIdx+1];
+    ll partSub = curSum - nums[curIdx+1];
+
+    bool canSum = partSum >=0 && partSum <= 20;
+    bool canSub = partSub >=0 && partSub <= 20;
+
+    if(canSum && canSub) return totalCase(curIdx+1, partSum) + totalCase(curIdx+1, partSub);
+    else if (canSum && !canSub) return totalCase(curIdx+1, partSum);
+    else if (!canSum && canSub) return totalCase(curIdx+1, partSub);
+    else return 0; // 계산이 안되는 경우. 현실적으로 없음
+}
+
+int main(){
+    cin >> N;
+
+    // init
+    for (int i=0;i<100;i++){
+        for (int j=0;j<2;j++){
+            subsum[i][j] = -1;
+        }
+    }
+    
+    for (int i=0; i<N; i++){
+        cin >> nums[i];
+    }
+    
+    cnt = totalCase(0, nums[0]);
+    cout << cnt << '\n';
+
+    return 0;
+}
