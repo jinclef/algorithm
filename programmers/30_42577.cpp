4cfeeb66778e3db9bbd463d77f6e05ffd1823826
성지원 <91617628+jinclef@users.noreply.github.com>
2025-04-25 18:15:51 +0900

    전화번호 목록

--- programmers/30_42577.cpp
+++ programmers/30_42577.cpp
@@ -1,43 +1,24 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
 #include <iostream>
 
 using namespace std;
-typedef pair<int, int> pii;
-vector<pii> sort_with_length;
-
-bool compare(pii a, pii b){
-    return a.second > b.second;
-}
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
+
+    int curr_idx = 0;
     
-    for(int i=0; i<phone_book.size(); i++){
-        sort_with_length.push_back(make_pair(i, phone_book[i].length())); // idx, len
-    }
-    
-    sort(sort_with_length.begin(), sort_with_length.end(), compare);
-    
-    int prefix_idx = phone_book.size()-1;
-    int word_idx = 0;
-    while(prefix_idx > word_idx){
-        string prefix = phone_book[sort_with_length[prefix_idx].first];
-        string word = phone_book[sort_with_length[word_idx].first];
-        
-        if(prefix != word && word.substr(0, prefix.length()) == prefix){ // 접두사 발견
+    while(curr_idx < phone_book.size()-1){
+        string prefix = phone_book[curr_idx];
+        string target = phone_book[curr_idx+1].substr(0, prefix.length());
+        if(prefix == target) {
             answer = false;
             break;
-        } else{
-            word_idx++;
-            if(word_idx == prefix_idx){
-                prefix_idx--;
-                word_idx = 0;
-            }
         }
+        curr_idx++;
     }
-    
     return answer;
 }
